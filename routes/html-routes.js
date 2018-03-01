f94fd25698f06f803bc00dcff3c5b4aaf37028bf
dcgutierrez93 <dcgutierrez93@yahoo.com>
2018-03-01 09:36:33 -0600

    Added html routes

---
+++ routes/html-routes.js
@@ -0,0 +1,16 @@
+// =============================================================================
+// Dependencies
+// =============================================================================
+var path = require("path");
+
+// Routes
+// =============================================================================
+module.exports = function(app) {
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname + "/../public/index.html"));
+    });
+
+    app.get("/home", function(req, res) {
+        res.sendFile(path.join(__dirname + "/../public/home.html"));
+    });
+};
--- server.js
+++ server.js
@@ -1,10 +1,28 @@
+// =============================================================================
+// Dependencies
+// =============================================================================
 var express = require("express");
+var bodyParser = require("body-parser");
+
 var db = require("./models");
 
 var PORT = process.env.PORT || 3000;
 var app = express();
 
-db.sequelize.sync().then(function() {
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Static Directory
+app.use(express.static("./public"));
+
+// Routes
+require("./routes/html-routes.js")(app);
+// require("./routes/api-routes.js")(app);
+
+db.sequelize.sync({ force: true }).then(function() {
     app.listen(PORT, function(){
         console.log("Listening on port %s", PORT);
     });
