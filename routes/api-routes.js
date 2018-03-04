acc7698a4fec2f8ed9248455322c044162a1eacc
dcgutierrez93 <dcgutierrez93@yahoo.com>
2018-03-04 13:37:01 -0600

    Added to form

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,17 +1,26 @@
 // =============================================================================
 // Dependencies
 // =============================================================================
+var express = require("express");
+var path    = require("path");
+
+// Create instance of our router.
+var router  = express.Router();
 
 // Require our user models
 var db = require("../models");
 
 // Routes
 // =============================================================================
-module.exports = function(app) {
+// module.exports = function(app) {
+//
+//     app.get("/api/posts", function(req, res) {
+//
+//     });
+//
+//
+// };
 
-    app.get("/api/posts", function(req, res) {
+router.post("/signup", function(req, res) {
 
-    });
-
-
-};
+});
